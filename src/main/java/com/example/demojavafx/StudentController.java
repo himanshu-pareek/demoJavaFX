d9ea2955e2334b22decbd24d142971d66abfd4ed
Himanshu Pareek <himanshupareekiit01@gmail.com>
2023-12-06 02:08:21 +0530

    Creates Students Screen using SceneBuilder.

--- src/main/java/com/example/demojavafx/StudentController.java
+++ src/main/java/com/example/demojavafx/StudentController.java
@@ -1,4 +1,60 @@
 package com.example.demojavafx;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class StudentController {
+
+    @FXML
+    private Button buttonAdd;
+
+    @FXML
+    private Button buttonDelete;
+
+    @FXML
+    private Button buttonUpdate;
+
+    @FXML
+    private TableColumn<?, ?> columnCourse;
+
+    @FXML
+    private TableColumn<?, ?> columnId;
+
+    @FXML
+    private TableColumn<?, ?> columnMobile;
+
+    @FXML
+    private TableColumn<?, ?> columnName;
+
+    @FXML
+    private TableView<?> tableStudents;
+
+    @FXML
+    private TextField textFieldCourse;
+
+    @FXML
+    private TextField textFieldMobile;
+
+    @FXML
+    private TextField textFieldName;
+
+    @FXML
+    void add(ActionEvent event) {
+
+    }
+
+    @FXML
+    void delete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void update(ActionEvent event) {
+
+    }
+
 }
