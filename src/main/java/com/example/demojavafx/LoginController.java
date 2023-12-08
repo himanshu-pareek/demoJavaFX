ef536d2282388dd44abc28a889f03edf315171dc
Himanshu Pareek <himanshupareekiit01@gmail.com>
2023-12-08 09:45:00 +0530

    Simpler change screen method.

--- src/main/java/com/example/demojavafx/LoginController.java
+++ src/main/java/com/example/demojavafx/LoginController.java
@@ -2,10 +2,16 @@ package com.example.demojavafx;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
 
@@ -18,28 +24,16 @@ public class LoginController {
     @FXML
     private Text loginMessage;
 
-    public void login(ActionEvent event) {
-        System.out.printf("Trying to login using %s:%s\n", username.getText(), password.getText());
-        loginMessage.setVisible(false);
-        try {
-            String loggedInUser = login(username.getText(), password.getText());
-            loginMessage.setText("Login Successful");
-            loginMessage.setStroke(Color.GREEN);
-        } catch (Exception e) {
-            loginMessage.setText(e.getMessage());
-            loginMessage.setStroke(Color.RED);
-        }
-        loginMessage.setVisible(true);
-    }
+    @FXML
+    private Button buttonLogin;
 
-    private String login(
-            String user,
-            String pass
-    ) throws Exception {
-        if (user.equals("john") || user.equals("jack")) {
-            return user;
-        } else {
-            throw new Exception("Invalid username / password");
-        }
+    public void login(ActionEvent event) throws IOException {
+        Scene currentScene = buttonLogin.getScene();
+        Stage stage = (Stage) currentScene.getWindow();
+        stage.setTitle("Dashboard");
+
+        FXMLLoader fxmlLoader = new FXMLLoader(DemoFXApplication.class.getResource("dashboard.fxml"));
+        Scene dashboardScene = new Scene(fxmlLoader.load());
+        stage.setScene(dashboardScene);
     }
 }
